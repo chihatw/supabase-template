739a33eaf1b2272911883b02d32b24bc4390d0e2
chihatw <chiha.tw@gmail.com>
2024-01-16 11:53:07 +0900

    login page change to RSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,24 +1,6 @@
-'use client';
-
 import LoginForm from '@/features/auth/components/LoginForm';
-import { createSupabaseClientComponentClient } from '@/lib/supabase';
-
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 const Login = () => {
-  const router = useRouter();
-  const supabase = createSupabaseClientComponentClient();
-
-  useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      if (session) {
-        router.refresh();
-      }
-    });
-    return () => data.subscription.unsubscribe();
-  }, [supabase, router]);
-
   return (
     <div>
       <div className='flex flex-col items-center text-2xl font-extralight pt-20 gap-1 text-gray-500'>
